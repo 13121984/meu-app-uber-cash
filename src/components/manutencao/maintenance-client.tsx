fb6bdef316f836f6d2433f31cda5479557eb7577
Paulo Vitor <pvitormc@gmail.com>
2025-09-04 00:17:01 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/manutencao/maintenance-client.tsx
+++ src/components/manutencao/maintenance-client.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useState, useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
 import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import {
