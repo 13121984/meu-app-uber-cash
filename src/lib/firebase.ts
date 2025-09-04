d5e7180c07e9abf54f20db254f30f441aca8bc5d
Paulo Vitor <pvitormc@gmail.com>
2025-09-04 01:50:55 +0000

    ainda nao aceitou

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,14 @@ import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
 import { getFirestore, Firestore } from "firebase/firestore";
 import { getAuth, Auth } from "firebase/auth";
 
+// As credenciais agora são lidas das variáveis de ambiente
 const firebaseConfig = {
-  "projectId": "app-pro-01-430113",
-  "appId": "1:1072979261835:web:2533c39958184f4f57d81b",
-  "storageBucket": "app-pro-01-430113.appspot.com",
-  "apiKey": "AIzaSyDFf3N5A_A4eL6DqGCEiE-r0jBIlSvaGS4",
-  "authDomain": "app-pro-01-430113.firebaseapp.com",
-  "messagingSenderId": "1072979261835"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Singleton pattern para inicialização do Firebase
@@ -17,14 +18,13 @@ let app: FirebaseApp;
 let auth: Auth;
 let db: Firestore;
 
-if (getApps().length) {
-    app = getApp();
-} else {
+if (!getApps().length) {
     app = initializeApp(firebaseConfig);
+} else {
+    app = getApp();
 }
 
 auth = getAuth(app);
 db = getFirestore(app);
-
 
 export { app, db, auth };
