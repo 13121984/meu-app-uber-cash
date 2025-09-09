9209e8314fcc59e0a03fc3cb72e37db661f7d52e
Paulo Vitor <pvitormc@gmail.com>
2025-09-09 20:56:14 +0000

    não era isso era o contrário. o icone do carrinho no icone de rota. se d

--- src/components/ui/app-logo.tsx
+++ src/components/ui/app-logo.tsx
@@ -14,11 +14,14 @@ export const AppLogo = ({ className }: { className?: string }) => {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className={cn("lucide-route", className)}
+      className={cn("lucide-car", className)}
     >
-        <circle cx="6" cy="19" r="3" />
-        <path d="M9 19h8.5a3.5 3.5 0 0 0 0-7h-11a3.5 3.5 0 0 1 0-7H15" />
-        <circle cx="18" cy="5" r="3" />
+      <path d="M19 17h2c.6 0 1-.4 1-1v-3c0-.9-.7-1.7-1.5-1.9C18.7 10.6 16 10 16 10s-1.3-1.4-2.2-2.3c-.5-.4-1.1-.7-1.8-.7H5c-.6 0-1.1.4-1.4.9L1 16v1c0 .6.4 1 1 1h2" />
+      <path d="M7 17h10" />
+      <path d="M5.9 17l-1.9-6" />
+      <path d="M18.1 17l1.9-6" />
+      <circle cx="7" cy="17" r="2" />
+      <circle cx="17" cy="17" r="2" />
     </svg>
   );
 };
