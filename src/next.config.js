99348d5b1b78fb3eae28aa21bb535fecf407edd5
Paulo Vitor <pvitormc@gmail.com>
2025-09-10 22:30:31 +0000

    certo pdf resolvido.agora esta dando um problema no download do csv. est

---
+++ src/next.config.js
@@ -0,0 +1,27 @@
+
+/** @type {import('next').NextConfig} */
+
+// Carrega as variáveis de ambiente do arquivo .env
+require('dotenv').config({ path: './.env' });
+
+const nextConfig = {
+  images: {
+    unoptimized: true, 
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
