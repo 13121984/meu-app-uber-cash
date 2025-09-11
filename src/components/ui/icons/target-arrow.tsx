0c1fa66cbb8ac0c7adece79ef11773d3f9ce5c1b
Paulo Vitor <pvitormc@gmail.com>
2025-09-11 19:01:17 +0000

    podemos usar esse?

--- src/components/ui/icons/target-arrow.tsx
+++ src/components/ui/icons/target-arrow.tsx
@@ -3,25 +3,29 @@ import { SVGProps } from 'react';
 
 export function IconTargetArrow(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}>
-        <g transform="rotate(-45 12 12)">
-            {/* Círculo externo vermelho */}
-            <circle cx="12" cy="12" r="10" fill="red"></circle>
-            {/* Círculo médio branco */}
-            <circle cx="12" cy="12" r="7" fill="white"></circle>
-            {/* Círculo interno vermelho */}
-            <circle cx="12" cy="12" r="4" fill="red"></circle>
-
-            {/* Haste da flecha */}
-            <path stroke="black" strokeWidth="2" d="M2,12 H22" />
-
-            {/* Penas da flecha (vermelhas) */}
-            <path fill="red" d="M19 8 l 4 4 l -4 4z" />
-            <path fill="red" d="M17 9 l 4 4 l -4 4z" />
-
-            {/* Ponta da flecha (Círculo preto no centro) */}
-            <circle cx="12" cy="12" r="1.5" fill="black"></circle>
-        </g>
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      width="1em" 
+      height="1em" 
+      viewBox="0 0 24 24" 
+      fill="none" 
+      stroke="currentColor" 
+      strokeWidth="2" 
+      strokeLinecap="round" 
+      strokeLinejoin="round" 
+      {...props}
+    >
+      {/* Outer Circle */}
+      <circle cx="12" cy="12" r="10" />
+      {/* Middle Circle */}
+      <circle cx="12" cy="12" r="6" />
+      {/* Inner Circle (Bullseye) */}
+      <circle cx="12" cy="12" r="2" />
+      
+      {/* Arrow */}
+      <path d="M12 18v-2" />
+      <path d="m15 11-3-3-3 3" />
+      <path d="M12 8V2" />
     </svg>
   );
 }
